989ca1c9a65edfcbc97ff84205647b215b9aa1a1
htdingkang <ht_dingkang@126.com>
2020-08-23 21:03:31 +0800

    外观模式（Facade）

---
+++ src/main/java/com/atguigu/designpattern/facade/Popcorn.java
@@ -0,0 +1,24 @@
+package com.atguigu.designpattern.facade;
+
+/**
+ * 爆米花机
+ */
+public class Popcorn {
+    private static Popcorn instance = new Popcorn();
+
+    public static Popcorn getInstance() {
+        return instance;
+    }
+
+    public void on() {
+        System.out.println(" popcorn on ");
+    }
+
+    public void off() {
+        System.out.println(" popcorn off ");
+    }
+
+    public void pop() {
+        System.out.println(" popcorn is poping  ");
+    }
+}
